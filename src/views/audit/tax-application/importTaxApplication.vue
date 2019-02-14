a5472700c6cdf1f31ea4d78fa38ecbabf4977d86
zhenghaiyang20062006@126.com <zhenghaiyang20062006@126.com>
2019-02-14 22:10:24 +0800

    ocean

--- src/views/audit/tax-application/importTaxApplication.vue
+++ src/views/audit/tax-application/importTaxApplication.vue
@@ -1008,6 +1008,7 @@ export default {
           applicantName: '',
           remarks: '',
           currentHandler: '',
+          
           financialReport: '',
           financialReportPath: '',
           fileName:"",
